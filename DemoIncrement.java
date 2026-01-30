695f9d34e56fed8c0644dd82bd675466e89cb6d3
mi365-dotcom <mi365@exeter.ac.uk>
2026-01-30 12:45:09 +0000

    popeyes

--- DemoIncrement.java
+++ DemoIncrement.java
@@ -5,11 +5,16 @@ public class DemoIncrement {
         int b = a++ + 5;
         int x = 3;
         int y = ++x + 5;
+        int p = 3;
+        ++p;
+        int q = p + 5; 
 
         System.out.print("a: " + a);
         System.out.print(", b: " + b);
         System.out.print(", x: " + x);
         System.out.println(", y: " + y);
+
+        System.out.println("a :" + a + ", b: " + b + ", x: " + x + ", y: " + y + ", p: " + p + ", q: " + q);
 
         // Replace the xxxxx in the following with the correct term
         System.out.println("i++ means post-increment");
