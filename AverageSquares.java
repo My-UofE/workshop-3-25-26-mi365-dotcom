b71f954f9c34a35ce2b7a6872cd941e19445e8aa
mi365-dotcom <mi365@exeter.ac.uk>
2026-01-30 15:07:14 +0000

    AverageSquares completed with while loop

---
+++ AverageSquares.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class AverageSquares {
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter the start value: ");
+        int startNum = in.nextInt();
+        
+
+        System.out.println("Please enter the end value: ");
+        int endNum = in.nextInt();
+        
+
+        System.out.println("You entered: " + startNum + " and " + endNum);
+
+        double sum = 0;
+        int count = 0;
+
+        while(startNum <= endNum){
+            sum += startNum * startNum;
+            count++;
+            startNum++;
+        }
+
+        double average = sum / count;
+
+        System.out.println("Average of squares from " + startNum + " to " + endNum + ":");
+        System.out.println(average);
+
+        System.out.println();
+
+
+    }
+    
+}
