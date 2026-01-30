884bd241def25256bccd49a4c1d175d91af6aceb
mi365-dotcom <mi365@exeter.ac.uk>
2026-01-30 14:11:52 +0000

    NumbersInSpanish completed with switch-case

--- NumbersInSpanish.java
+++ NumbersInSpanish.java
@@ -6,16 +6,22 @@ public class NumbersInSpanish {
        switch(number) {
         case 1:
             System.out.println("uno");
+            break;
         case 2:
             System.out.println("dos");
+            break;
         case 3:
             System.out.println("tres");
+            break;
         case 4:
             System.out.println("cuatro");
+            break;
         case 5:
             System.out.println("cinco");
+            break;
         default:
             System.out.println("Sorry I don't know that");
+
        }
 
 
