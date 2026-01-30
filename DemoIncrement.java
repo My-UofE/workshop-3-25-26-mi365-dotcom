d87ba70330cd61f5f5c6fa9aa2c4a373d2f2c033
mi365-dotcom <mi365@exeter.ac.uk>
2026-01-30 12:17:44 +0000

    cocacola

--- DemoIncrement.java
+++ DemoIncrement.java
@@ -12,8 +12,8 @@ public class DemoIncrement {
         System.out.println(", y: " + y);
 
         // Replace the xxxxx in the following with the correct term
-        System.out.println("i++ means xxxxx-increment");
-        System.out.println("++i means xxxxx-increment");
+        System.out.println("i++ means post-increment");
+        System.out.println("++i means pre-increment");
     }
 }// Add your code below
 
