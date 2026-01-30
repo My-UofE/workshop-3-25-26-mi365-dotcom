f31c445e8bca02851b1633cb6785de21bdecfdd1
mi365-dotcom <mi365@exeter.ac.uk>
2026-01-30 13:20:53 +0000

    AgeToGroup completed with ternary operator

--- AgeToGroup.java
+++ AgeToGroup.java
@@ -2,12 +2,8 @@ public class AgeToGroup {
     public static void main(String[] args){
         int age = Integer.parseInt(args[0]);
 
-        int ageGroup = 1;
-        if (age > 18)
-            ageGroup = 0;
-        
-        if (age>=65)
-            ageGroup = 2;
+
+        int ageGroup = (age<=18)?0:(age=>)?2:1
 
         System.out.println("Age: " + age + ", Age Group: " + ageGroup);
     }
